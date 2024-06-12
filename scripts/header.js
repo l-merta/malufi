0e4fef9c0b7dc370487c98ad8b0dde7893f58cc2
Mertosaur <164738919+Mertosaur@users.noreply.github.com>
2024-06-12 20:19:53 +0200

    Add files via upload

---
+++ scripts/header.js
@@ -0,0 +1,16 @@
+const header = document.querySelector('header');
+const firstWhite = document.querySelector('main').scrollHeight - (10 * 16) - (6 * 16);
+//console.log(firstWhite);
+
+window.addEventListener('scroll', function() {
+  console.log("scroll");
+  console.log(window.pageYOffset, firstWhite);
+  if (window.pageYOffset > firstWhite) {
+    header.style = "color: var(--c1);";
+    header.querySelector("nav .spec").style = "border-color: var(--c1);";
+  } else {
+    header.style = "color: var(--c2);";
+    header.querySelector("nav .spec").style = "border-color: var(--c2);";
+  }
+});
+//document.addEventListener('resize', ()=>{sticky = header.offsetTop;});
