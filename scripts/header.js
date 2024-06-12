d521d15fefa0061f4aee6805b37fa102d93a6672
Mertosaur <164738919+Mertosaur@users.noreply.github.com>
2024-06-12 19:50:55 +0000

    push

--- scripts/header.js
+++ scripts/header.js
@@ -1,13 +1,13 @@
 const header = document.querySelector('header');
 const firstWhite = document.querySelector('main').scrollHeight - (10 * 16) - (6 * 16);
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', recolorHeader);
+recolorHeader();
+function recolorHeader() {
   if (window.pageYOffset > firstWhite) {
-    header.classList = "header-dark";
-    header.querySelector("nav .spec").style = "border-color: var(--c1);";
-  } else {
     header.classList = "header-light";
-    header.querySelector("nav .spec").style = "border-color: var(--c2);";
+  } else {
+    header.classList = "header-dark";
   }
-});
+}
 //document.addEventListener('resize', ()=>{sticky = header.offsetTop;});
