9d17717efe1fefa6e9cc860ebaf94ad298cc63de
Mertosaur <164738919+Mertosaur@users.noreply.github.com>
2024-06-12 19:45:48 +0000

    push

--- scripts/header.js
+++ scripts/header.js
@@ -1,15 +1,12 @@
 const header = document.querySelector('header');
 const firstWhite = document.querySelector('main').scrollHeight - (10 * 16) - (6 * 16);
-//console.log(firstWhite);
 
 window.addEventListener('scroll', function() {
-  console.log("scroll");
-  console.log(window.pageYOffset, firstWhite);
   if (window.pageYOffset > firstWhite) {
-    header.style = "color: var(--c1);";
+    header.classList = "header-dark";
     header.querySelector("nav .spec").style = "border-color: var(--c1);";
   } else {
-    header.style = "color: var(--c2);";
+    header.classList = "header-light";
     header.querySelector("nav .spec").style = "border-color: var(--c2);";
   }
 });
