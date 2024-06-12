d4feb7728248165b5dee7a2d036a8c75a3d2a214
f-radosta <168348485+f-radosta@users.noreply.github.com>
2024-06-12 20:15:49 +0000

    novej skript

--- scripts/header.js
+++ scripts/header.js
@@ -1,18 +1,49 @@
-const header = document.querySelector('header');
-const firstWhite = document.querySelector('main').scrollHeight - (10 * 16) - (6 * 16);
-//console.log(firstWhite);
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+  const main = document.querySelector('main');
+  let firstWhite = calculateFirstWhite(main);
 
-window.addEventListener('scroll', function() {
-  console.log("scroll");
-  console.log(window.pageYOffset, firstWhite);
-  if (window.pageYOffset > firstWhite) {
-    header.style = "color: var(--c1);";
-    header.querySelector("nav .spec").style = "border-color: var(--c1);";
-  } else {
-    header.style = "color: var(--c2);";
-    header.querySelector("nav .spec").style = "border-color: var(--c2);";
+  window.addEventListener('scroll', () => {
+    handleScroll(header, firstWhite);
+  });
+
+  window.addEventListener('resize', () => {
+    firstWhite = calculateFirstWhite(main);
+  });
+
+  function calculateFirstWhite(mainElement) {
+    return mainElement.scrollHeight - (10 * 16) - (6 * 16);
+  }
+
+  function handleScroll(headerElement, threshold) {
+    if (window.scrollY > threshold) {
+      headerElement.classList.add('scrolled');
+      headerElement.classList.remove('unscrolled');
+    } else {
+      headerElement.classList.add('unscrolled');
+      headerElement.classList.remove('scrolled');
+    }
   }
 });
-//document.addEventListener('resize', ()=>{sticky = header.offsetTop;});
 
-// novej koment
+
+
+
+// const header = document.querySelector('header');
+// const firstWhite = document.querySelector('main').scrollHeight - (10 * 16) - (6 * 16);
+// //console.log(firstWhite);
+
+// window.addEventListener('scroll', function() {
+//   console.log("scroll");
+//   console.log(window.pageYOffset, firstWhite);
+//   if (window.pageYOffset > firstWhite) {
+//     header.style = "color: var(--c1);";
+//     header.querySelector("nav .spec").style = "border-color: var(--c1);";
+//   } else {
+//     header.style = "color: var(--c2);";
+//     header.querySelector("nav .spec").style = "border-color: var(--c2);";
+//   }
+// });
+// //document.addEventListener('resize', ()=>{sticky = header.offsetTop;});
+
+// // novej koment
