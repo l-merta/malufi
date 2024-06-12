24e6e0104f2f31b219fb670c07b09e2a310bdf56
Mertosaur <164738919+Mertosaur@users.noreply.github.com>
2024-06-12 21:08:27 +0000

    weby 01

---
+++ scripts/weby.js
@@ -0,0 +1,18 @@
+const webCont = document.querySelector(".weby .cont");
+const webs = [
+  "https://unsplash.it/2560/1440",
+  "https://unsplash.it/2561/1441",
+  "https://unsplash.it/2562/1442",
+  "https://unsplash.it/2563/1443",
+  "https://unsplash.it/2564/1444"
+];
+///
+
+renderWebs();
+function renderWebs() {
+  for(let i = 0; i < webs.length; i++) {
+    const nWeb = document.createElement('div');
+    nWeb.classList = "web";
+    webCont.appendChild(nWeb, webCont.children[webCont.children.length]);
+  }
+}
