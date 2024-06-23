b8cb9980f489964170287710b4bb2f933d53e1b4
Mertosaur <164738919+Mertosaur@users.noreply.github.com>
2024-06-23 19:32:17 +0000

    push

--- scripts/jakToFunguje.js
+++ scripts/jakToFunguje.js
@@ -3,7 +3,7 @@ const line = lineCont.querySelector(".line");
 const innerLine = line.querySelector(".innerLine");
 
 function updateInnerLine() {
-  let height = (line.getBoundingClientRect().top - (window.innerHeight/2 - 50)) * -1;
+  let height = (line.getBoundingClientRect().top - (window.innerHeight/2 + 150)) * -1;
   if(height < 0)
     height = 0;
   innerLine.style.height = height+0+"px";
