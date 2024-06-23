23075c7aceff3dd4e2691c99b39fc88b7dadd1f5
Mertosaur <164738919+Mertosaur@users.noreply.github.com>
2024-06-23 11:00:43 +0000

    push

--- scripts/weby.js
+++ scripts/weby.js
@@ -8,6 +8,13 @@ const webs = [
   "https://unsplash.it/2563/1443",
   "https://unsplash.it/2564/1444"
 ];
+const webColors = [
+  "var(--c6)",
+  "var(--c6)",
+  "var(--c6)",
+  "var(--c6)",
+  "var(--c6)",
+]
 ///
 
 renderWebs();
@@ -17,18 +24,22 @@ function renderWebs() {
   //web na začátek
   const nWebStart = slides.querySelector('.webDef').cloneNode(true);
   nWebStart.classList = "web edgeWeb";
-  nWebStart.querySelector("img").src = webs[webs.length-1];
+  nWebStart.querySelector(".img-back").style = "background-color: "+webColors[webs.length-1];//webs[webs.length-1];
+  nWebStart.querySelector(".img-front").src = webs[webs.length-1];
   slides.appendChild(nWebStart, slides.children[1]);
   ///
   for(let i = 0; i < webs.length; i++) {
     const nWeb = slides.querySelector('.webDef').cloneNode(true);
     nWeb.classList = "web";
-    nWeb.querySelector("img").src = webs[i];
+    nWeb.querySelector(".img-back").style = "background-color: "+webColors[i];//webs[i];
+    nWeb.querySelector(".img-front").src = webs[i];
     slides.appendChild(nWeb, slides.children[slides.children.length]);
   }
   ///web na konec
   const nWebEnd = slides.querySelector('.webDef').cloneNode(true);
   nWebEnd.classList = "web edgeWeb";
+  nWebEnd.querySelector(".img-back").style = "background-color: "+webColors[0];//webs[0];
+  nWebEnd.querySelector(".img-front").src = webs[0];
   nWebEnd.querySelector("img").src = webs[0];
   slides.appendChild(nWebEnd, slides.children[slides.children.length]);
 }
