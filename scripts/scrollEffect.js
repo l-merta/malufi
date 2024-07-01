0b115790b46188a7ad6a60c1a96fa123b1216303
Mertosaur <164738919+Mertosaur@users.noreply.github.com>
2024-07-01 20:56:29 +0000

    push

--- scripts/scrollEffect.js
+++ scripts/scrollEffect.js
@@ -5,6 +5,6 @@ pjsbg.push(document.querySelector("#particles-js-bg-r"));
 window.addEventListener('scroll', () => {
   const scrollValue = window.scrollY;
   console.log(scrollValue);
-  pjsbg[0].style = "top: "+(scrollValue * 0.3)+"px;";
-  pjsbg[1].style = "top: "+(scrollValue * 0.3)+"px;";
+  pjsbg[0].style = "top: "+(scrollValue * 0.2)+"px;";
+  pjsbg[1].style = "top: "+(scrollValue * 0.2)+"px;";
 });
