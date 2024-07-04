22e65a15b746156db38a7f45cd94629c7532c82c
Mertosaur <164738919+Mertosaur@users.noreply.github.com>
2024-07-04 11:12:29 +0000

    push

--- scripts/scrollEffect.js
+++ scripts/scrollEffect.js
@@ -2,8 +2,19 @@ const pjsbg = []; //particles-js-bg
 pjsbg.push(document.querySelector("#particles-js-bg-l"));
 pjsbg.push(document.querySelector("#particles-js-bg-r"));
 
-window.addEventListener('scroll', () => {
-  const scrollValue = window.scrollY;
-  pjsbg[0].style = "top: "+(scrollValue * 0.4)+"px;";
-  pjsbg[1].style = "top: "+(scrollValue * 0.4)+"px;";
-});
+updateScrollEffect();
+function updateScrollEffect() {
+  if(screen.height > screen.width) {
+    //mobil
+    const scrollValue = screen.height;
+    pjsbg[0].style = "top: "+(scrollValue * 0.85)+"px;";
+    pjsbg[1].style = "top: "+(scrollValue * 0.85)+"px;";
+  }
+  else {
+    //kompl
+    const scrollValue = window.scrollY;
+    pjsbg[0].style = "top: "+(scrollValue * 0.4)+"px;";
+    pjsbg[1].style = "top: "+(scrollValue * 0.4)+"px;";
+  }
+}
+window.addEventListener('scroll', updateScrollEffect);
