924c0a9353dafef34a017e9a7b97903a7e072591
Mertosaur <164738919+Mertosaur@users.noreply.github.com>
2024-07-04 10:47:17 +0000

    push

--- scripts/scrollEffect.js
+++ scripts/scrollEffect.js
@@ -4,7 +4,6 @@ pjsbg.push(document.querySelector("#particles-js-bg-r"));
 
 window.addEventListener('scroll', () => {
   const scrollValue = window.scrollY;
-  console.log(scrollValue);
   pjsbg[0].style = "top: "+(scrollValue * 0.4)+"px;";
   pjsbg[1].style = "top: "+(scrollValue * 0.4)+"px;";
 });
--- scripts/weby.js
+++ scripts/weby.js
@@ -2,16 +2,16 @@ const slides = document.querySelector(".weby .slides");
 const dots = document.querySelector(".weby .dots");
 
 const webs = [
-  "https://unsplash.it/2560/1440",
-  "https://unsplash.it/2561/1441",
-  "https://unsplash.it/2562/1442",
+  "images/anglictina.png",
+  "images/fitnes.png",
+  "images/melisa.png",
   "https://unsplash.it/2563/1443",
   "https://unsplash.it/2564/1444"
 ];
 const webColors = [
-  "var(--c6)",
-  "var(--c6)",
-  "var(--c6)",
+  "images/anglictina_pozadi.png",
+  "images/fitnes_pozadi.png",
+  "images/melisa_pozadi.png",
   "var(--c6)",
   "var(--c6)",
 ]
@@ -24,21 +24,21 @@ function renderWebs() {
   //web na začátek
   const nWebStart = slides.querySelector('.webDef').cloneNode(true);
   nWebStart.classList = "web edgeWeb";
-  nWebStart.querySelector(".img-back").style = "background-color: "+webColors[webs.length-1];//webs[webs.length-1];
+  nWebStart.querySelector(".img-back").style = "background: url("+webColors[webs.length-1]+");";//webs[webs.length-1];
   nWebStart.querySelector(".img-front").src = webs[webs.length-1];
   slides.appendChild(nWebStart, slides.children[1]);
   ///
   for(let i = 0; i < webs.length; i++) {
     const nWeb = slides.querySelector('.webDef').cloneNode(true);
     nWeb.classList = "web";
-    nWeb.querySelector(".img-back").style = "background-color: "+webColors[i];//webs[i];
+    nWeb.querySelector(".img-back").style = "background: url("+webColors[i]+");";//webs[i];
     nWeb.querySelector(".img-front").src = webs[i];
     slides.appendChild(nWeb, slides.children[slides.children.length]);
   }
   ///web na konec
   const nWebEnd = slides.querySelector('.webDef').cloneNode(true);
   nWebEnd.classList = "web edgeWeb";
-  nWebEnd.querySelector(".img-back").style = "background-color: "+webColors[0];//webs[0];
+  nWebEnd.querySelector(".img-back").style = "background: url("+webColors[0]+");";//webs[0];
   nWebEnd.querySelector(".img-front").src = webs[0];
   nWebEnd.querySelector("img").src = webs[0];
   slides.appendChild(nWebEnd, slides.children[slides.children.length]);
