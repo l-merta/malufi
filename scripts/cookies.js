ed347003fac89452d78a28a5e212e9a277b8896d
Mertosaur <164738919+Mertosaur@users.noreply.github.com>
2024-07-06 20:54:35 +0000

    push

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,3 +1,13 @@
-function zapnoutCookies() {
+const cookie = document.querySelector(".cookie");
+
+function zapnoutCookie() {
   //tady :D
+  console.log("zapnuto");
+}
+function showCookie(state) {
+  console.log(state);
+  if(state)
+    cookie.classList = "cookie";
+  else 
+    cookie.classList = "cookie cookie-close";
 }
