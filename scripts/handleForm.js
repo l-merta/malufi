cfdb8522c5584a031f6c348506a40fdd941a77dd
Mertosaur <164738919+Mertosaur@users.noreply.github.com>
2024-07-13 15:59:20 +0000

    push

--- scripts/handleForm.js
+++ scripts/handleForm.js
@@ -3,8 +3,15 @@ const errorMsg = document.querySelector("#errorMsg");
 
 kontaktniFormular.addEventListener("submit", function (event) {
   event.preventDefault();
+  if(document.querySelector(".submitCont").classList.contains("submitCont-load"))
+    return;
+
   const formData = new FormData(kontaktniFormular);
   errorMsg.innerText = "";
+  //
+  kontaktniFormular.querySelector(".submit").value = "";
+  kontaktniFormular.querySelector(".submitCont").classList.add("submitCont-load");
+  //
 
   fetch("php/contactForm.php", {
     method: "POST",
@@ -26,10 +33,14 @@ kontaktniFormular.addEventListener("submit", function (event) {
 function handleSuccess() {
   //event.preventDefault();
   document.querySelector(".formCont").classList.add("formCont-sent")
+  kontaktniFormular.querySelector(".submit").value = "Odeslat";
+  kontaktniFormular.querySelector(".submitCont").classList.remove("submitCont-load");
 }
 function handleError(errMsg) {
   //event.preventDefault();
-  console.error("Error:", errMsg);
+  //console.error("Error: " + errMsg);
   errorMsg.innerText = "Chyba, zkuste to znovu.";
   errorMsg.style.display = "block";
+  kontaktniFormular.querySelector(".submit").value = "Odeslat";
+  kontaktniFormular.querySelector(".submitCont").classList.remove("submitCont-load");
 }
